34441714819f357c1b10b69a52e0276fd22cec79
Vani <160762020+vanixjnk@users.noreply.github.com>
2024-09-24 21:24:25 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,16 @@
+// next.config.js
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-
-    images: {
-        domains: ['th.bing.com', 'image.icons8.com', 'cdn-icons-png.flaticon.com', 'github.githubassets.com', 'github.com', 'upload.wikimedia.org', 'stc-zaloprofile.zdn.vn'],
-    },
-
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*", // Cho phép mọi hostname
+        port: "",
+      },
+    ],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
